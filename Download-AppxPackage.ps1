9e9fa741a75bc018c2004292b962a5ea7905dcbf
Brandon Fingerhut <brandonfingerhut@gmail.com>
2021-10-19 16:00:43 -0500

    Basic Working Script

---
+++ Download-AppxPackage.ps1
@@ -0,0 +1,33 @@
+[CmdletBinding()]
+param (
+  [string]$Uri,
+  [string]$Path = "."
+)
+  $StopWatch = [system.diagnostics.stopwatch]::startnew()
+  $Path = (Resolve-Path $Path).Path
+  
+  # Get Urls to download
+  Write-Host -ForegroundColor Yellow "Processing $Uri"
+  $WebResponse = Invoke-WebRequest -UseBasicParsing -Method 'POST' -Uri 'https://store.rg-adguard.net/api/GetFiles' -Body "type=url&url=$Uri&ring=Retail" -ContentType 'application/x-www-form-urlencoded'
+  $LinksMatch = ($WebResponse.Links | where {$_ -like '*.appx*'} |  Select-String -Pattern '(?<=a href=").+(?=" r)').matches.value
+    $Files = ($WebResponse.Links | where {$_ -like '*.appx*'} | Select-String -Pattern '(?<=noreferrer">).+(?=</a>)').matches.value
+  
+  #Create array of links and filenames
+  $DownloadLinks = @()
+  for($i = 0;$i -lt $LinksMatch.Count; $i++){
+    $Array += ,@($LinksMatch[$i],$Files[$i])
+  }
+  
+  #Sort by filename descending
+  $Array = $Array | sort-object @{Expression={$_[1]}; Descending=$True}
+  $LastFile = "temp123"
+  for($i = 0;$i -lt $LinksMatch.Count; $i++){
+    $CurrentFile = $Array[$i][1]
+    $CurrentUrl = $Array[$i][0]
+    "Downloading $Path\$CurrentFile"
+    $FilePath = "$Path\$CurrentFile"
+    $FileRequest = Invoke-WebRequest -Uri $CurrentUrl -UseBasicParsing #-Method Head
+    [System.IO.File]::WriteAllBytes($FilePath, $FileRequest.content)
+
+  }
+"Time to process: "+$StopWatch.ElapsedMilliseconds
